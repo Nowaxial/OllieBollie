4e281f3ce5bfcc08e86240a9c7a1daecb19d36a5
Maria Toledo <Nowaxial@users.noreply.github.com>
2023-06-12 03:37:52 +0200

    Making everything nice at last
    Updated the ParentingPage

--- src/components/parenting/parentingComponent.tsx
+++ src/components/parenting/parentingComponent.tsx
@@ -1,151 +1,111 @@
-/* import { Box, Card, Skeleton, Text, Title } from '@mantine/core';
+import { Box, Card, Center, Text, Title } from '@mantine/core';
 
 function ParentingComponent() {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <center>
+      <Center>
         <Title order={2} align="center" style={{ marginBottom: '1rem' }} fw={700}>
-          Snart
+          Föräldraskapet, en utmaning?
         </Title>
-      </center>
-      <Skeleton visible={false}>
-        <Box style={{ marginTop: '1rem' }}>
-          <Text style={{ marginBottom: '0.5rem' }} fw={500} td="underline" italic fz="lg">
-            Inledning:
-          </Text>
-          <Text>
-            Att kommunicera med spädbarn och små barn är viktigt för deras utveckling och
-            välbefinnande. Även om de inte kan prata än, kan de använda olika sätt att
-            kommunicera med oss. Som förälder eller vårdgivare kan du använda enkla
-            strategier för att förstå och kommunicera med ditt barn. I denna artikel
-            kommer vi att titta på hur du kan använda TAKK-tecken och andra enkla metoder
-            för att kommunicera med små barn.
-          </Text>
-          <Text>
-            TAKK-tecken är användbara verktyg för att hjälpa små barn att kommunicera. Här
-            är några exempel på tecken:
-          </Text>
-        </Box>
+      </Center>
+      <Box style={{ marginTop: '1rem' }}>
+        <Text>
+          Föräldraskap är en av de mest utmanande och belönande uppgifterna i livet. Att
+          uppfostra ett barn kräver tålamod, kärlek och engagemang. Det kan också vara
+          stressigt och utmanande, särskilt när man hanterar sömnbrist, känslor och
+          skilsmässa. Men det finns många sätt att hantera dessa utmaningar och stärka
+          ditt föräldraskap. En av de största utmaningarna för föräldrar är stress. Att
+          hantera stress är viktigt för att undvika utbrändhet och för att kunna ge ditt
+          barn den uppmärksamhet och kärlek som det behöver. Stress kan orsakas av många
+          faktorer, inklusive arbete, ekonomi, relationer och föräldraskap. Föräldraskap
+          kan vara särskilt stressigt, särskilt när man hanterar sömnbrist och utmanande
+          beteenden från barnet. Det finns många sätt att hantera stress som förälder. En
+          av de viktigaste sakerna du kan göra är att ta pauser. Det kan vara svårt att
+          hitta tid för dig själv när du har ett barn att ta hand om, men det är viktigt
+          att du tar tid för att koppla av och ladda upp. Det kan inkludera att ta en
+          promenad, läsa en bok eller träna. Träning är också en viktig del av att hantera
+          stress. Träning frigör endorfiner som kan hjälpa till att minska stress och
+          ångest. Det kan vara svårt att hitta tid för träning när du har ett barn att ta
+          hand om, men det finns många sätt att inkludera träning i din dagliga rutin. Det
+          kan inkludera att ta en promenad med barnvagnen, att träna hemma eller att gå
+          med i en träningsgrupp för föräldrar. Att få stöd från andra föräldrar är också
+          viktigt för att hantera stress. Att prata med andra föräldrar som går igenom
+          samma utmaningar kan hjälpa till att minska stress och ge dig nya perspektiv.
+          Det kan inkludera att gå med i en föräldragrupp, att prata med vänner eller att
+          söka professionellt stöd.
+        </Text>
+      </Box>
+      <Box style={{ marginTop: '1rem' }}>
+        <Text>
+          En annan viktig aspekt av föräldraskap är att hjälpa ditt barn att hantera sina
+          känslor. Barn kan ha svårt att uttrycka sina känslor och det är viktigt att ge
+          dem verktyg för att hantera dem på ett hälsosamt sätt. Det kan inkludera att
+          prata med dem om deras känslor, ge dem utlopp för sina känslor genom lek eller
+          konst och att visa dem hur man hanterar stress och oro. Att hjälpa ditt barn att
+          utveckla känslomässig intelligens är också viktigt för deras hälsa och
+          välbefinnande. Känslomässig intelligens handlar om att kunna känna igen, förstå
+          och hantera sina egna känslor och andras känslor. Det kan inkludera att lära
+          ditt barn att identifiera olika känslor, att hjälpa dem att uttrycka sina
+          känslor och att visa dem hur man hanterar konflikter på ett hälsosamt sätt.
+          Sömnbrist är en annan utmaning för föräldrar, särskilt när barnet är spädbarn
+          eller småbarn. Att få tillräckligt med sömn är viktigt för både föräldrar och
+          barns hälsa och välbefinnande. Sömnbrist kan orsaka trötthet, irritabilitet och
+          minskad koncentration. Det finns många sätt att hantera sömnbrist som förälder.
+          En av de viktigaste sakerna du kan göra är att skapa en rutin för sömn. Det kan
+          inkludera att lägga ditt barn vid samma tid varje kväll, att skapa en lugn och
+          avslappnande miljö för sömn och att undvika stimulerande aktiviteter innan
+          läggdags. Att ta pauser är också viktigt för att hantera sömnbrist. Det kan
+          inkludera att be om hjälp från andra föräldrar eller att anställa en barnvakt
+          för att få lite tid för dig själv. Att få stöd från andra föräldrar är också
+          viktigt för att hantera sömnbrist. Att prata med andra föräldrar som går igenom
+          samma utmaningar kan hjälpa till att minska stress och ge dig nya perspektiv.
+        </Text>
+      </Box>
+      <Box style={{ marginTop: '1rem' }}>
+        <Text>
+          Att stärka ditt barns självkänsla är också en viktig del av föräldraskapet. Att
+          ha en positiv självbild är viktigt för att barn ska kunna hantera utmaningar och
+          motgångar i livet. Det kan inkludera att ge dem positiv feedback, att uppmuntra
+          dem att prova nya saker och att visa dem att du tror på dem. Att ge ditt barn
+          möjlighet att ta ansvar och fatta egna beslut är också viktigt för att stärka
+          deras självkänsla. Det kan inkludera att låta dem välja sina egna kläder eller
+          att låta dem bestämma vad de vill äta till middag. Att ge dem möjlighet att ta
+          ansvar och fatta egna beslut kan hjälpa dem att känna sig självsäkra och
+          självständiga. Skilsmässa kan vara en svår tid för både föräldrar och barn. Det
+          är viktigt att stötta ditt barn genom denna tid och att hjälpa dem att hantera
+          sina känslor. Det kan inkludera att prata med dem om vad som händer, att ge dem
+          tid att bearbeta sina känslor och att visa dem att du finns där för dem. Att ha
+          en öppen och ärlig kommunikation med ditt barn är också viktigt under en
+          skilsmässa. Det kan inkludera att svara på deras frågor och att ge dem
+          information om vad som händer. Att visa dem att du finns där för dem och att du
+          bryr dig om deras känslor kan hjälpa dem att känna sig trygga och stödda.
+        </Text>
+      </Box>
 
-        <Box style={{ marginTop: '1rem' }}>
-          <center>
-            <Title order={4} align="center" style={{ marginBottom: '1rem' }} fw={700}>
-              Här är några sätt barn kan använda TAKK-tecken:
-            </Title>
-          </center>
-          <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-            1. Förståelse av ord:
-          </Text>
+      <Box style={{ marginTop: '2rem' }}>
+        <Text
+          variant="h3"
+          style={{ marginBottom: '0.5rem' }}
+          fw={500}
+          td="underline"
+          fz="lg"
+          italic
+        >
+          Sammanfattning:
+        </Text>
+        <Box style={{ marginTop: '1.5rem' }}>
           <Text>
-            TAKK-tecken kan hjälpa barn att förstå olika ord och deras betydelser. Genom
-            att använda tecknen tillsammans med ord kan barnet koppla ihop tecknet med
-            ordet och förstå dess mening.
+            Sammanfattningsvis är föräldraskap en utmanande men belönande uppgift. Att
+            hantera stress, hjälpa ditt barn att hantera sina känslor, hantera sömnbrist,
+            stärka deras självkänsla och stötta dem genom svåra tider är alla viktiga
+            aspekter av föräldraskapet. Genom att använda dessa verktyg kan du stärka ditt
+            föräldraskap och ge ditt barn den kärlek och uppmärksamhet som det behöver för
+            att växa och utvecklas.
           </Text>
         </Box>
-
-        <Box style={{ marginTop: '1rem' }}>
-          <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-            2. Enkel kommunikation:
-          </Text>
-          <Text>
-            Små barn kan använda enkla TAKK-tecken för att uttrycka sina behov och
-            önskemål. Till exempel kan de använda tecknet för "mer" när de vill ha mer mat
-            eller tecknet för "tack" när de är tacksamma. Genom att använda tecken kan de
-            visa vad de vill även om de inte kan prata ännu.
-          </Text>
-        </Box>
-
-        <Box style={{ marginTop: '1rem' }}>
-          <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-            3. Minska frustration:
-          </Text>
-          <Text>
-            TAKK-tecken kan hjälpa till att minska frustrationen hos små barn som ännu
-            inte kan prata. Genom att använda tecken kan de kommunicera vad de vill eller
-            behöver, vilket kan minska frustrationen över att inte bli förstådda. Det kan
-            också ge dem en känsla av självständighet och att de kan göra sig förstådda.
-          </Text>
-        </Box>
-
-        <Box style={{ marginTop: '1rem' }}>
-          <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-            4. Skapa anknytning och interaktion:
-          </Text>
-          <Text>
-            När du lär dig TAKK-tecken och använder dem tillsammans med ditt barn, kan ni
-            skapa en starkare anknytning. Genom att använda tecken kan ditt barn känna sig
-            förstått och kan kommunicera mer direkt med dig. Det kan stärka er relation
-            och förståelse för varandra.
-          </Text>
-        </Box>
-
-        <Box style={{ marginTop: '1rem' }}>
-          <Text style={{ marginBottom: '0.5rem' }} fw={500} italic>
-            Förutom TAKK-tecken finns det också andra enkla sätt att kommunicera med små
-            barn:
-          </Text>
-          <Box style={{ marginTop: '1rem' }}>
-            <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-              Kroppsspråk och ansiktsuttryck:
-            </Text>
-            <Text>
-              Små barn förstår mycket genom kroppsspråk och ansiktsuttryck. Genom att
-              använda leenden, ögonkontakt och olika rörelser kan du kommunicera med ditt
-              barn utan ord.
-            </Text>
-          </Box>
-          <Box style={{ marginTop: '1rem' }}>
-            <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-              Lyssna och svara:
-            </Text>
-            <Text>
-              Det är viktigt att vara en lyhörd lyssnare och svara på ditt barns signaler
-              och behov. Genom att vara uppmärksam och visa att du förstår deras känslor,
-              hjälper du ditt barn att känna sig tryggt och förstått.
-            </Text>
-          </Box>
-          <Box style={{ marginTop: '1rem' }}>
-            <Text variant="h3" style={{ marginBottom: '0.5rem' }} fw={500}>
-              Lek och interaktion:
-            </Text>
-            <Text>
-              Lek är en naturlig och viktig del av kommunikationen med små barn. Genom att
-              leka tillsammans och interagera får ditt barn möjlighet att utforska och
-              uttrycka sina tankar och känslor. Det är också ett sätt att bygga en stark
-              relation och ha roligt tillsammans.
-            </Text>
-          </Box>
-        </Box>
-
-        <Box style={{ marginTop: '2rem' }}>
-          <Text
-            variant="h3"
-            style={{ marginBottom: '0.5rem' }}
-            fw={500}
-            td="underline"
-            fz="lg"
-            italic
-          >
-            Sammanfattning:
-          </Text>
-          <Box style={{ marginTop: '2rem' }}>
-            <Text>
-              Att kommunicera med små barn är viktigt för deras utveckling och relationer.
-              TAKK-tecken kan vara en användbar metod för att hjälpa barn att förstå och
-              uttrycka sig. Genom att använda enkla strategier som kroppsspråk, lyssnande
-              och lek kan du skapa en meningsfull kommunikation med ditt barn. Kom ihåg
-              att vara närvarande och engagerad i deras kommunikationsvärld för att stödja
-              deras språkutveckling och skapa en stark anknytning.
-            </Text>
-            <Text variant="h3" style={{ marginTop: '0.5rem' }}>
-              Nedanför kommer det 2 videos som visar hur man kan lära små barn TAKK.
-            </Text>
-          </Box>
-        </Box>
-      </Skeleton>
+      </Box>
     </Card>
   );
 }
 
 export default ParentingComponent;
- */
--- src/pages/ParentingPage.tsx
+++ src/pages/ParentingPage.tsx
@@ -5,7 +5,7 @@ import ParentingComponent from '../components/parenting/parentingComponent';
 const ParentingPage = () => {
   return (
     <Container size="md" style={{ marginTop: '2rem' }}>
-      <div>Föräldraskapet</div>
+      <ParentingComponent />
     </Container>
   );
 };
